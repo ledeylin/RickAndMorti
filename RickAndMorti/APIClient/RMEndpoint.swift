9abe65aafc7dc20875d38abbf9a56715cdf93e86
ledeylin <ledeylin@mail.ru>
2024-06-12 11:16:24 +0500

    Added docstrings and API related files

---
+++ RickAndMorti/APIClient/RMEndpoint.swift
@@ -0,0 +1,15 @@
+//
+//  RMEndpoint.swift
+//  RickAndMorti
+//
+//  Created by Лера on 12.06.2024.
+//
+
+import Foundation
+
+/// Pepresents unique API endpoint
+@frozen enum Endpoint: String {
+    case character
+    case location
+    case episode
+}
---
+++ RickAndMorti/APIClient/RMRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RMRequest.swift
+//  RickAndMorti
+//
+//  Created by Лера on 12.06.2024.
+//
+
+import Foundation
+
+/// Object that represents a singlet API call
+final class RMRequest {
+    
+}
